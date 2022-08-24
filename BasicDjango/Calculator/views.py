710a594844bcccea4f444d328c63933fb0423c07
Vishal Chaurasiya <vishal8study@gmail.com>
2022-08-24 12:05:47 +0530

    addition operation done

--- BasicDjango/Calculator/views.py
+++ BasicDjango/Calculator/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name': 'Vishal Chaurasiya'})
+
+
+def add(request):
+
+    res = int(request.GET["num1"]) + int(request.GET["num2"])
+
+    return render(request, 'result.html', { 'result': res })
